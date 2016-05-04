8923112ebae5c8b690eb8e9b275b7e3ccb8cf2ff
Artyom Goncharov <m4yers@gmail.com>
2016-05-04 12:10:13 +0200

    Move to another ws

---
+++ src/test/java/com/blah/app/TestSolversValidity.java
@@ -0,0 +1,21 @@
+package com.blah.app;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import java.util.Collections;
+import java.util.Arrays;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+//TODO extend with edge test cases
+public class TestSolversValidity {
+
+    @Test
+    public void testValidity() {
+    }
+}
